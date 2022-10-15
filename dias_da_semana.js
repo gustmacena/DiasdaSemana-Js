4924924e3c275e903bc9d422023483ec32b60d1c
gustmacena <102830712+gustmacena@users.noreply.github.com>
2022-10-14 21:07:06 -0300

    Dias da Semana

---
+++ dias_da_semana.js
@@ -0,0 +1,19 @@
+let num = "4"
+
+if(num ==1){
+    console.log("Domingo")
+} else if(num == 2){
+    console.log("segunda")
+} else if(num == 3){
+    console.log("terca") 
+} else if(num == 4){
+    console.log("quarta") 
+} else if(num == 5){
+    console.log("quinta") 
+} else if(num == 6){
+    console.log("sexta") 
+} else if(num == 7){
+    console.log("sabado") 
+} else {
+    console.log("numero invalido") 
+} 
